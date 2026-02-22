fb4404eee49c40efead9eed0e68ce8a7de2bd96e
Rohit Singh <rsrohitsingh931@gmail.com>
2026-02-23 03:00:14 +0530

    feat: add header component with game controls, progress display, and foundational global styling.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, HostListener, ViewChild, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SudokuService, Difficulty } from '../../services/sudoku/sudoku.service';
 
@@ -10,18 +10,27 @@ import { SudokuService, Difficulty } from '../../services/sudoku/sudoku.service'
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  @ViewChild('dropdown') dropdownRef!: ElementRef;
   sudokuService = inject(SudokuService);
   gameState$ = this.sudokuService.gameState$;
 
   isMenuOpen = false;
   themes = ['Dark', 'Light', 'Sapphire'];
 
-  toggleMenu() {
+  toggleMenu(event: Event) {
+    event.stopPropagation();
     this.isMenuOpen = !this.isMenuOpen;
   }
 
   setDifficulty(diff: Difficulty) {
     this.sudokuService.startNewGame(diff);
     this.isMenuOpen = false;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    if (this.isMenuOpen && this.dropdownRef && !this.dropdownRef.nativeElement.contains(event.target as Node)) {
+      this.isMenuOpen = false;
+    }
   }
 }
